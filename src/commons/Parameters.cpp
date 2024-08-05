54a7f5c556a97682a0de63d9042bd2d19581d6ee
Joey Lee <leejoey0921@gmail.com>
2024-08-06 01:26:44 +0900

    adding comments

--- src/commons/Parameters.cpp
+++ src/commons/Parameters.cpp
@@ -2513,6 +2513,7 @@ void Parameters::setDefaults() {
     resultDirection = Parameters::PARAM_RESULT_DIRECTION_TARGET;
     weightThr = 0.9;
     weightFile = "";
+    hashSeqBuffer = 1.5;
 
     // result2stats
     stat = "";
